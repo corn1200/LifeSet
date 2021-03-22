1319a5df96e15c7d2d0c948fe362b93f5283d70c
corn1200 <corn120033@gmail.com>
2021-03-22 18:32:28 +0900

    feat: 노티피케이션을 사용하여 클릭 횟수를 출력한다

--- app/src/main/java/com/example/lifeset/MainActivity.java
+++ app/src/main/java/com/example/lifeset/MainActivity.java
@@ -1,14 +1,72 @@
 package com.example.lifeset;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String NOTIFICATION_CHANNEL_ID = "10001";
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                NotificationSomethings();
+            }
+        });
+    }
+
+    public void NotificationSomethings() {
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent notificationIntent = new Intent(this, ResultActivity.class);
+        notificationIntent.putExtra("notificationId", count);
+        notificationIntent.setFlags
+                (Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder
+                (this, NOTIFICATION_CHANNEL_ID).setLargeIcon
+                (BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground))
+                .setContentTitle("상태바 드래그시 보이는 타이틀")
+                .setContentText("상태바 드래그시 보이는 서브타이틀")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent).setAutoCancel(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+            CharSequence channelName = "노티피케이션 채널";
+            String description = "오레오 이상을 위한 것임";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+
+            NotificationChannel channel = new NotificationChannel
+                    (NOTIFICATION_CHANNEL_ID, channelName, importance);
+
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(channel);
+        } else {
+            builder.setSmallIcon(R.mipmap.ic_launcher);
+        }
+
+        assert notificationManager != null;
+        notificationManager.notify(1234, builder.build());
     }
 }
