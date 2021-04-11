2fd40c46c968eb2e50bde7b8895011490cd194d0
corn1200 <corn120033@gmail.com>
2021-04-11 20:06:04 +0900

    add: 알람에 대한 동작을 담당하는 서비스 파일을 추가한다

--- app/src/main/java/com/example/lifeset/ImportantAlarmActivity.java
+++ app/src/main/java/com/example/lifeset/ImportantAlarmActivity.java
@@ -1,4 +1,14 @@
 package com.example.lifeset;
 
-public class ImportantAlarmActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ImportantAlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_importantalarm);
+    }
 }
